671210fdf5e8abc483149f2167cee66b1052ac29
gradonion <zhouyf@uchicago.edu>
2022-02-28 03:28:46 -0600

    T cell gsfa

--- R/prepossess_TCells.R
+++ R/prepossess_TCells.R
@@ -3,120 +3,139 @@ library(tidyverse)
 library(Matrix)
 library(Seurat)
 library(GSFA)
-## Download "GSE142078_RAW.tar" from GEO: GSE142078 and decompress it
+## Download "GSE119450_RAW.tar" from GEO: GSE119450 and decompress it
 ## Change the following data directory to where that folder is:
-data_dir <- "/project2/xinhe/yifan/Factor_analysis/LUHMES/GSE142078_raw/"
+data_dir <- "/project2/xinhe/yifan/Factor_analysis/Stimulated_T_Cells/GSE119450_RAW/"
 
-filename_tb <- data.frame(experiment = c("Run1", "Run2", "Run3"),
-                          prefix = c("GSM4219575_Run1", "GSM4219576_Run2", "GSM4219577_Run3"),
-                          stringsAsFactors = F)
+filename_tb <- 
+  data.frame(experiment = c("D1S", "D2S", "D1N", "D2N"),
+             prefix = c("GSM3375488_D1S", "GSM3375490_D2S", "GSM3375487_D1N", "GSM3375489_D2N"),
+             stringsAsFactors = F)
 seurat_lst <- list()
 guide_lst <- list()
-for (i in 1:3){
+
+for (i in 1:4){
   experiment <- filename_tb$experiment[i]
   prefix <- filename_tb$prefix[i]
   print(paste0("Loading data of ", experiment))
   
-  feature.names <- data.frame(fread(paste0(data_dir, prefix, "_genes.tsv.gz"),
+  feature.names <- data.frame(fread(paste0(data_dir, experiment, "/genes.tsv"),
                                     header = FALSE), stringsAsFactors = FALSE)
-  barcode.names <- data.frame(fread(paste0(data_dir, prefix, "_barcodes.tsv.gz"),
+  barcode.names <- data.frame(fread(paste0(data_dir, experiment, "/barcodes.tsv"),
                                     header = FALSE), stringsAsFactors = FALSE)
   barcode.names$V2 <- sapply(strsplit(barcode.names$V1, split = "-"),
                              function(x){x[1]})
   # Load the gene count matrix (gene x cell) and annotate the dimension names:
-  dm <- readMM(file = paste0(data_dir, prefix, "_matrix.mtx.gz"))
+  dm <- readMM(file = paste0(data_dir, experiment, "/matrix.mtx"))
   rownames(dm) <- feature.names$V1
   colnames(dm) <- barcode.names$V2
   print(dim(dm))
+  
   # Load the meta data of cells:
-  metadata <- data.frame(fread(paste0(data_dir, prefix, "_Cell_Guide_Lookup.csv.gz"),
+  metadata <- data.frame(fread(paste0(data_dir, experiment, "/",
+                                      prefix, "_CellBC_sgRNA.csv.gz"),
                                header = T, sep = ','), check.names = F)
-  metadata$target <- sapply(strsplit(metadata$sgRNA, split = "_"),
-                            function(x){x[1]})
-  metadata <- metadata %>% filter(CellBarcode %in% barcode.names$V2)
-  targets <- unique(metadata$target)
+  # Example of "metadata" content:
+  #            Cell.BC                   gRNA.ID sample.name UMI.count
+  # 1 AAAACAAACTGCAGCC ES.sg42.NonTarget.CTRL125     D1_Stim         1
+  # 2 AAACCTGAGAGCTGGT              ES.sg21.LAG3     D1_Stim         1
+  # 3 AAACCTGAGATACACA           ES.sg5.C10orf54     D1_Stim         1
+  metadata$gene_target <- sapply(strsplit(metadata$gRNA.ID, split = "[.]"),
+                                 function(x){x[3]})
+  metadata$guide <- sapply(strsplit(metadata$gRNA.ID, split = "[.]"),
+                           function(x){paste0(x[2], ".", x[3])})
+  print(nrow(metadata))
+  metadata <- metadata %>% filter(Cell.BC %in% barcode.names$V2)
+  targets <- unique(metadata$gene_target)
   targets <- targets[order(targets)]
   # Make a cell by perturbation matrix:
   guide_mat <- data.frame(matrix(nrow = nrow(metadata),
                                  ncol = length(targets)))
-  rownames(guide_mat) <- metadata$CellBarcode
+  rownames(guide_mat) <- metadata$Cell.BC
   colnames(guide_mat) <- targets
-  for (i in targets){
-    guide_mat[[i]] <- (metadata$target == i) * 1
+  for (m in targets){
+    guide_mat[[m]] <- (metadata$gene_target == m) * 1
   }
   guide_lst[[experiment]] <- guide_mat
   
   # Only keep cells with gRNA info:
-  dm.cells_w_gRNA <- dm[, metadata$CellBarcode]
-  real_gene_bool <- startsWith(rownames(dm.cells_w_gRNA), "ENSG")
-  dm.cells_w_gRNA <- dm.cells_w_gRNA[real_gene_bool, ]
+  dm.cells_w_gRNA <- dm[, metadata$Cell.BC]
   print("Dimensions of final gene expression matrix:")
   print(dim(dm.cells_w_gRNA))
   
-  dm.seurat <- CreateSeuratObject(dm.cells_w_gRNA, project = paste0("LUHMES_", experiment))
+  dm.seurat <- CreateSeuratObject(dm.cells_w_gRNA, project = paste0("TCells_", experiment))
   dm.seurat <- AddMetaData(dm.seurat, metadata = guide_mat)
   seurat_lst[[experiment]] <- dm.seurat
 }
 
-combined_obj <- merge(seurat_lst[[1]], c(seurat_lst[[2]], seurat_lst[[3]]),
-                      add.cell.ids = c("Run1", "Run2", "Run3"),
-                      project = "LUHMES")
+combined_obj <- merge(seurat_lst[[1]], 
+                      c(seurat_lst[[2]], seurat_lst[[3]], seurat_lst[[4]]),
+                      add.cell.ids = filename_tb$experiment,
+                      project = "T_cells_all_merged")
 print("Dimensions of merged gene expression matrix:")
-print(dim(combined_obj))
+dim(combined_obj)
 
 # Seurat QC ####
 MT_genes <- feature.names %>% filter(startsWith(V2, "MT-")) %>% pull(V1)
 combined_obj[['percent_mt']] <- PercentageFeatureSet(combined_obj, features = MT_genes)
 VlnPlot(combined_obj, features = c('nFeature_RNA','nCount_RNA','percent_mt'), pt.size = 0.3)
-combined_obj <- subset(combined_obj, subset = percent_mt < 10 & nCount_RNA < 20000)
-VlnPlot(combined_obj, features = c('nFeature_RNA','nCount_RNA','percent_mt'), pt.size = 0.3)
+combined_obj <- subset(combined_obj, subset = percent_mt < 10 & nFeature_RNA > 500)
 
 print("Dimensions of merged gene expression matrix after QC:")
-print(dim(combined_obj))
+dim(combined_obj)
+# [1] 33694 24955
 
 # Deviance residual transformation ####
 dev_res <- deviance_residual_transform(t(as.matrix(combined_obj@assays$RNA@counts)))
 top_gene_index <- select_top_devres_genes(dev_res, num_top_genes = 6000)
-dev_res_filtered <- dev_res[, top_gene_index]
+dev_res_filtered <- dev_res[, top_gene_index] # 24955 x 6000 matrix
 
-# Regress out batches, library size, unique UMI count, and MT-gene percentage:
-covariate_df <- data.frame(batch = factor(combined_obj$orig.ident),
-                           lib_size = combined_obj$nCount_RNA,
+## Regress out library size, unique UMI count, and MT-gene percentage:
+covariate_df <- data.frame(lib_size = combined_obj$nCount_RNA,
                            umi_count = combined_obj$nFeature_RNA,
                            percent_mt = combined_obj$percent_mt)
 dev_res_corrected <- covariate_removal(dev_res_filtered, covariate_df)
 
-# Cell x gene normalized expression matrix for GSFA input (Y):
+## Cell x gene normalized expression matrix for GSFA input (Y):
 scaled.gene_exp <- scale(dev_res_corrected)
 sample_names <- colnames(combined_obj@assays$RNA@counts)
 gene_names <- rownames(combined_obj@assays$RNA@counts)
 rownames(scaled.gene_exp) <- sample_names
 colnames(scaled.gene_exp) <- gene_names[top_gene_index]
 
-# Cell x perturbation matrix for GSFA input (G):
-G_mat <- combined_obj@meta.data[, 4:18]
+## Cell x perturbation matrix for GSFA input (G):
+G_mat <- combined_obj@meta.data[, 4:24]
 G_mat <- as.matrix(G_mat)
-# saveRDS(G_mat, "../data/LUHMES/perturbation_matrix.rds")
+# saveRDS(G_mat, "../data/TCells/perturbation_matrix.rds")
 
-# Number of cells with each perturbed gene:
+## Number of cells with each perturbed gene:
 colSums(G_mat)
+
+# Cell group info:
+group <- combined_obj$orig.ident
+# saveRDS(group, "../data/TCells/sample_groups.rds")
+group <- (group %in% c("TCells_D1S", "TCells_D2S")) * 1
+## For two group GSFA,
+## Group 0: unstimulated cells, Group 1: stimulated cells
 
 
 # Seurat visualization (just to explore the data, not necessary for GSFA) ####
-gene_detection <- rowMeans(combined_obj@assays$RNA@counts > 0)
 combined_obj <- NormalizeData(combined_obj, normalization.method = "LogNormalize", scale.factor = 1e4)
+gene_detection <- rowMeans(combined_obj@assays$RNA@counts > 0)
+kept_features <- names(gene_detection)[gene_detection > 0.1]
 combined_obj <- ScaleData(combined_obj,
-                          features = names(gene_detection)[gene_detection > 0.1],
-                          vars.to.regress = c("orig.ident", "nFeature_RNA", "nCount_RNA", "percent_mt"))
+                          features = kept_features,
+                          vars.to.regress = c("nFeature_RNA", "nCount_RNA", "percent_mt"))
 
-## PCA + UMAP
+## PCA + UMAP:
 combined_obj <- RunPCA(combined_obj, features = kept_features, verbose = F)
 ElbowPlot(combined_obj, ndims = 20)
 combined_obj <- FindNeighbors(combined_obj, reduction = "pca")
-combined_obj <- RunUMAP(combined_obj, reduction = "pca", dims = 1:8)
+combined_obj <- RunUMAP(combined_obj, reduction = "pca", dims = 1:10)
 
-## Batch effect
-DimPlot(combined_obj, reduction = "umap", group.by = "orig.ident")
+## Donor and stimulation effects:
+DimPlot(combined_obj, reduction = "umap", group.by = "orig.ident",
+        cols = c("red3", "steelblue4", "forestgreen", "orchid4"))
 
 ## Cell cycle effects
 CC_genes_df <- data.frame(fread("/project2/xinhe/shared_data/cell_cycle_genes.csv"))
@@ -130,7 +149,7 @@ for (i in 1:ncol(CC_genes_df)){
 FeaturePlot(combined_obj, features = names(CC_genes_df))
 
 ## Marker gene expression
-marker_genes <- c("TOP2A", "NEUROD1" , "STMN2", "MAP2")
+marker_genes <- c("IL7R", "CCR7" , "MKI67", "GZMB")
 for (m in marker_genes){
   gene_id <- feature.names$V1[feature.names$V2 == m]
   combined_obj[[m]] <- norm.rna[gene_id, ]
@@ -151,22 +170,21 @@ theme_set(theme_bw() + theme(plot.title = element_text(size = 14, hjust = 0.5),
 library(gridExtra)
 library(grid)
 
-targets <- names(combined_obj@meta.data)[4:18]
-plot_data <- DimPlot(combined_obj, reduction = 'umap', group.by = 'Nontargeting')
+targets <- names(combined_obj@meta.data)[4:24]
+plot_data <- DimPlot(combined_obj, reduction = 'umap', group.by = 'NonTarget')
 plot.lst <- list()
-for (i in targets){
+for (i in names(guide_mat)){
   plot_data$data[[i]] <- factor(combined_obj[[i]][, 1])
   p <- ggplot(plot_data$data, aes_string(x = "UMAP_1", y = "UMAP_2", color = i)) +
     geom_point(alpha = 0.6, size = 0.3) +
     scale_color_manual(values = c("lightgrey", "red")) +
     labs(title = i) +
     theme(legend.position = "None",
-          axis.title = element_blank(),
-          panel.grid = element_blank())
+          axis.title = element_blank())
   plot.lst[[i]] <- p
 }
-args <- c(plot.lst, list(nrow = 3,
-                         left = textGrob("UMAP 1", rot = 90, vjust = 0.5,
-                                         gp = gpar(fontsize = 14, fontface = 'bold')),
-                         bottom = textGrob("UMAP 2", gp = gpar(fontsize = 14, fontface = 'bold'))))
+args <- c(plot.lst,
+          list(left = textGrob("UMAP 1", rot = 90, vjust = 0.5,
+                               gp = gpar(fontsize = 14, fontface = 'bold')),
+               bottom = textGrob("UMAP 2", gp = gpar(fontsize = 14, fontface = 'bold'))))
 do.call(grid.arrange, args)
--- R/run_gsfa_TCells_2groups.R
+++ R/run_gsfa_TCells_2groups.R
@@ -1,13 +1,15 @@
 ## Recommend to run this script on a computing cluster that allows for
-## ~ 50GB memory and ~ 3 hours of no interruption.
+## ~ 50GB memory and ~ 5.5 hours of no interruption.
 ## GSFA results were generated under default parameters with the output of
-## "scaled.gene_exp" and "G_mat" from "preprocess_LUHMES.R" as GSFA input.
-## Gibbs sampling was performed for 3000 iterations in one run,
-## but can also be carried out in segments by setting "niter" to a smaller number
-## and resuming the sampling using the "restart" option
-## if only limited run time and memory can be allocated.
-## Three output files:
-## A full Gibbs sampling object; posterior mean estimates of parameters; an LFSR matrix.
+## "scaled.gene_exp", "G_mat" and "group" from "preprocess_TCells.R" as GSFA input.
+## Gibbs sampling was performed for 2000 iterations in a first run,
+## and then carried on for another 2000 iterations in a second run
+## using the "restart" option.
+## If limited run time and memory can be allocated, set "niter" to a smaller
+## number and run more segments.
+## Four output files:
+## A full Gibbs sampling object; posterior mean estimates of parameters; 
+## two LFSR matrices (one for each cell group).
 library(GSFA)
 library(optparse)
 option_list <- list(
@@ -18,6 +20,10 @@ option_list <- list(
   make_option(
     "--perturbation_file", action = "store", default = NA, type = 'character',
     help = "RDS file of sample by perturbation matrix for GSFA input (G) [required]"
+  ),
+  make_option(
+    "--sample_group_file", action = "store", default = NA, type = 'character',
+    help = "RDS file of sample group information for GSFA by group [required]"
   ),
   make_option(
     "--out_folder", action = "store", default = NA, type = 'character',
@@ -52,7 +58,7 @@ option_list <- list(
     help = "Number of factors to use, default is %default [auto-detected when restart=TRUE]"
   ),
   make_option(
-    "--niter", action = "store", default = 3000, type = 'integer',
+    "--niter", action = "store", default = 2000, type = 'integer',
     help = "Number of iterations to sample, default is %default"
   ),
   make_option(
@@ -68,20 +74,27 @@ opt <- parse_args(OptionParser(option_list = option_list))
 
 expression_file <- opt$expression_file
 perturbation_file <- opt$perturbation_file
+sample_group_file <- opt$sample_group_file
 out_folder <- opt$out_folder
-init_method <- opt$init_method
+init_method <- opt$init_method # "svd"
 out_suffix <- opt$out_suffix
-K <- opt$K
+K <- opt$K # 20
 niter <- opt$niter
-average_niter <- opt$average_niter
-random_seed <- opt$random_seed
+average_niter <- opt$average_niter # 1000
+random_seed <- opt$random_seed # 92629
 
 stopifnot(file.exists(expression_file) & file.exists(perturbation_file) &
-            dir.exists(out_folder))
+            file.exists(sample_group_file) & dir.exists(out_folder))
 print("Loading input data ...")
 scaled.gene_exp <- readRDS(expression_file)
+scaled.gene_exp <- t(scaled.gene_exp)
 G_mat <- readRDS(perturbation_file)
-stopifnot(rownames(G_mat) == rownames(scaled.gene_exp))
+sample_group <- readRDS(sample_group_file)
+stopifnot(rownames(scaled.gene_exp) == rownames(G_mat) & 
+            rownames(G_mat) == names(sample_group))
+
+sample_group <- (sample_group %in% c("TCells_D1S", "TCells_D2S")) * 1
+## 0: unstimulated, 1: stimulated
 
 if (opt$restart){
   stopifnot(file.exists(opt$previous_res))
@@ -90,19 +103,22 @@ if (opt$restart){
 
 if (!opt$permute){
   set.seed(random_seed)
-  out_dir <- paste0(out_folder, "gibbs_obj_k", K, ".", out_suffix, ".seed_", random_seed, ".rds")
+  out_dir <- paste0(out_folder, "gibbs_obj_k", K, ".", out_suffix, ".rds")
 } else {
   stopifnot(opt$perm_num %in% 1:10)
   seeds <- c(49553, 72704, 11932, 56826, 49707, 33357, 93747, 95392, 96675, 38186)
   random_seed <- seeds[opt$perm_num]
   print(paste("Permuting cell orders under seed", random_seed, "..."))
   set.seed(random_seed)
-  
-  new_cell_order <- sample(nrow(scaled.gene_exp))
-  scaled.gene_exp <- scaled.gene_exp[new_cell_order, ]
+  ## Permutation within each cell group:
+  new_cell_order <- rep(NA, ncol(scaled.gene_exp))
+  new_cell_order[which(sample_group==0)] <- gtools::permute(which(sample_group==0))
+  new_cell_order[which(sample_group==1)] <- gtools::permute(which(sample_group==1))
+  scaled.gene_exp <- scaled.gene_exp[, new_cell_order]
   out_dir <- paste0(out_folder, "gibbs_obj_k", K, ".", paste0("perm_", opt$perm_num), 
                     ".", out_suffix, ".rds")
 }
+
 if (file.exists(out_dir)){
   warnings("Output file exists and will be overwritten!")
 }
@@ -110,30 +126,38 @@ if (file.exists(out_dir)){
 if (opt$restart == F){
   print(paste0("Performing GSFA with ", K, " factors and ", niter, " iterations."))
   print(paste0("Results will be saved at ", out_dir))
-  fit <- fit_gsfa_multivar(Y = scaled.gene_exp, G = G_mat, K = K,
-                           prior_type = "mixture_normal", init.method = init_method,
-                           prior_w_s = 50, prior_w_r = 0.2,
-                           prior_beta_s = 20, prior_beta_r = 0.2,
-                           niter = niter, used_niter = average_niter,
-                           verbose = T, return_samples = T)
+  fit <- fit_gsfa_multivar_2groups(Y = scaled.gene_exp, G = G_mat, 
+                                   group = sample_group, K = K,
+                                   prior_type = "mixture_normal", init.method = init_method,
+                                   prior_w_s = 50, prior_w_r = 0.2,
+                                   prior_beta_s = 20, prior_beta_r = 0.2,
+                                   niter = niter, used_niter = average_niter,
+                                   verbose = T, return_samples = T)
 } else {
   print(paste0("Resuming GSFA on top of a previous result saved at ", opt$previous_res,
                " for ", niter, " iterations."))
   print(paste0("Results will be saved at ", out_dir))
-  fit <- fit_gsfa_multivar(Y = scaled.gene_exp, G = G_mat, fit0 = prev_fit,
-                           prior_type = "mixture_normal", init.method = init_method,
-                           prior_w_s = 50, prior_w_r = 0.2,
-                           prior_beta_s = 20, prior_beta_r = 0.2,
-                           niter = niter, used_niter = average_niter,
-                           verbose = T, return_samples = T)
+  fit <- fit_gsfa_multivar_2groups(Y = scaled.gene_exp, G = G_mat, 
+                                   group = sample_group, fit0 = prev_fit,
+                                   prior_type = "mixture_normal", init.method = init_method,
+                                   prior_w_s = 50, prior_w_r = 0.2,
+                                   prior_beta_s = 20, prior_beta_r = 0.2,
+                                   niter = niter, used_niter = average_niter,
+                                   verbose = T, return_samples = T)
 }
 print("Finished! Saving the results...")
 saveRDS(fit, file = out_dir)
 
 saveRDS(fit$posterior_means, file = sub(pattern = ".rds", replacement = ".PM.rds", x = out_dir))
 
-lfsr_mat <- fit$lfsr
-lfsr_mat <- lfsr_mat[, -ncol(lfsr_mat)]
-print("# of genes that pass LFSR < 0.05:")
-print(colSums(lfsr_mat < 0.05))
-saveRDS(lfsr_mat, file = sub(pattern = ".rds", replacement = ".lfsr_mat.rds", x = out_dir))
+lfsr0_mat <- fit$lfsr0
+lfsr0_mat <- lfsr0_mat[, -ncol(lfsr0_mat)]
+print("Group 0, # of genes that pass LFSR < 0.05:")
+print(colSums(lfsr0_mat < 0.05))
+saveRDS(lfsr0_mat, file = sub(pattern = ".rds", replacement = ".lfsr_mat.group0.rds", x = out_dir))
+  
+lfsr1_mat <- fit$lfsr1
+lfsr1_mat <- lfsr1_mat[, -ncol(lfsr1_mat)]
+print("Group 1, # of genes that pass LFSR < 0.05 (computed from all factors):")
+print(colSums(lfsr1_mat < 0.05))
+saveRDS(lfsr1_mat, file = sub(pattern = ".rds", replacement = ".lfsr_mat.group1.rds", x = out_dir))
